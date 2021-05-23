40900c9255ef4583d90c34c675ad6982200524e5
Patryk <77505092+brzezik919@users.noreply.github.com>
2021-05-23 21:01:00 +0200

    Java Script + Card Panel Interactive editForm after click edit button

--- src/main/resources/static/css/general.js
+++ src/main/resources/static/css/general.js
@@ -4,4 +4,12 @@ function createDivAddCard() {
                 div.innerHTML = document.getElementById('addForm').innerHTML;
                 document.getElementById('addCard').appendChild(div);
         }
-}n
+}
+function createDivEditCard() {
+        if(document.querySelector("#editCard").innerHTML.trim() == ""){
+                let div = document.createElement('div');
+                div.innerHTML = document.getElementById('editForm').innerHTML;
+                document.getElementById('editCard').appendChild(div);
+        }
+        document.getElementById("editName").innerHTML += 'SIEMA';
+}
